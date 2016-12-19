54be94c268d243ef214a02cde429d07204b28358
sylvesterharvey <metagumi@gmail.com>
2016-12-19 11:32:42 -0500

    Changes # 1

---
+++ CriminalIntent/app/src/main/java/com/metagumi/sylvesterharvey/criminalintent/Crime.java
@@ -0,0 +1,29 @@
+package com.metagumi.sylvesterharvey.criminalintent;
+
+import java.util.UUID;
+
+/**
+ * Created by sylvesterharvey on 12/19/16. model
+ */
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public Crime() {
+        // Generate unique identifier
+        mId = UUID.randomUUID();
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+}
