e19d7d159ca2726b6c6d3286fada31b1510b137e
sylvesterharvey <metagumi@gmail.com>
2016-12-20 17:43:39 -0500

    Changes # 5

--- CriminalIntent/app/src/main/java/com/metagumi/sylvesterharvey/criminalintent/CrimeActivity.java
+++ CriminalIntent/app/src/main/java/com/metagumi/sylvesterharvey/criminalintent/CrimeActivity.java
@@ -2,7 +2,9 @@ package com.metagumi.sylvesterharvey.criminalintent;
 
 // model layer
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,5 +14,15 @@ public class CrimeActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 }
